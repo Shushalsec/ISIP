a23094e356dfeed4ab8df754bf7193c8e8fb3833
Shushan Toneyan <shtoneyan@gmail.com>
2018-05-06 16:39:53 +0200

    applied RANSAC for the image series

--- imageImport.py
+++ imageImport.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu May  3 06:30:32 2018
 
 @author: Shushan
+
+This script reads the images provided in the directory into a 4D numpy array.
+To use it simply call the function StoreImages and give 1 argument - the absolute path
+of the directory
 """
+
 
 import numpy as np
 import os
 from scipy.misc import imread
 from os.path import isfile, join
-import matplotlib.pyplot as plt
 
 
 
@@ -17,20 +22,18 @@ def getImageNames(imgPath):
     return [f for f in os.listdir(imgPath) if isfile(join(imgPath, f))]
 
 
-file_dir_a = 'project_data\\project_data\\a'
-cwd = os.getcwd()
+DATA_DIR = 'C:\\Users\\Shushan\\Desktop\\Mission Masters\\ISIP\\Group Project\\project_data\\project_data\\a'
 
-DATA_DIR = os.path.join(cwd, file_dir_a)
+def StoreImages(directory):
+    img_names = getImageNames(directory) #extract image file names into a list
+    img0 = imread(os.path.join(directory, img_names[0]), mode='RGB') #read the first image
+    dim1, dim2, dim3 = img0.shape #get the dimensions of the result array
+    img_frames = np.zeros(shape=(dim1, dim2, dim3, len(img_names))) #create an empty array
+    for index, img in enumerate(img_names): #loop over images and get the count of the image too
+        img_frames[:,:,:,index] = imread(os.path.join(directory, img), mode='RGB') # save the 3D array in the correspoinding 4th D index
+    return img_frames #return the 4D array
 
 
 
-
-
-def main():
-    img_names = getImageNames(DATA_DIR)
-    img_frames = np.zeros(shape=(480, 640, 3, len(img_names)))
-    for index, img in enumerate(img_names):
-        img_frames[:,:,:,index] = imread(os.path.join(DATA_DIR, img), mode='RGB')
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
