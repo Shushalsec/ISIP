06df00a9f1859f42a887850a97e9bbd8a28f3073
Judith <judithbergada@gmail.com>
2018-05-07 19:29:04 +0200

    More manicure

--- NCC.py
+++ NCC.py
@@ -7,11 +7,13 @@
 # Shushan Toneyan, Madleina Caduff, Judith Bergada Pijuan
 
 import os
-import numpy as np
-import matplotlib.pyplot as plt
 from glob import glob
-from skimage.feature import match_template
+
+import matplotlib.pyplot as plt
+import numpy as np
 from skimage.color import rgb2gray
+from skimage.feature import match_template
+
 
 def imgsimplify(img, c, window):
     """
@@ -31,10 +33,12 @@ def imgsimplify(img, c, window):
     """
     c_x, c_y = c[0], c[1]
     window_half = (window - 1) // 2
-    simple_img = img[c_y-window_half:c_y+window_half+1, c_x-window_half:c_x+window_half+1]
+    simple_img = img[c_y - window_half:c_y + window_half + 1,
+                     c_x - window_half:c_x + window_half + 1]
     return simple_img
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     #_______________________________________________________________
     # Activate to work only with set A
     files_names = glob("./project_data/a/*.png")
@@ -52,12 +56,13 @@ if __name__== '__main__':
     # Parameters
     num_imgs = 10
     window_next = 35
-    window_template = window_next//2
+    window_template = window_next // 2
 
     # Initial conditions
-    img = rgb2gray(plt.imread(files_names[0])) # read first image with known starting positions
+    img = rgb2gray(plt.imread(
+        files_names[0]))  # read first image with known starting positions
     previous_imgs = np.zeros((num_imgs, window_template, window_template))
-    previous_imgs[0,:,:] = imgsimplify(img, c_in, window_template)
+    previous_imgs[0, :, :] = imgsimplify(img, c_in, window_template)
 
     # Open a .txt file where the coordinates of the points will be writen
     output.write("  image_name\t    x-location\t    y-location\n")
@@ -65,16 +70,17 @@ if __name__== '__main__':
     output.flush()
 
     # Find the desired pixel in all frames
-    for i in range(1,len(files_names)):
+    for i in range(1, len(files_names)):
         # Take next image of the list and its potential regions to find the point
         img_to_plot = plt.imread(files_names[i])
-        img_next = rgb2gray(img_to_plot) # convert to grayscale
+        img_next = rgb2gray(img_to_plot)  # convert to grayscale
         simple_imgnext = imgsimplify(img_next, c_in, window_next)
 
         # Compute NCC considering the previous frames as templates
         NCC_max = 0
-        for template in previous_imgs[0:min(i,len(previous_imgs))]:
-            NCC_obtained = match_template(simple_imgnext, template, pad_input=True, mode="edge")
+        for template in previous_imgs[0:min(i, len(previous_imgs))]:
+            NCC_obtained = match_template(
+                simple_imgnext, template, pad_input=True, mode="edge")
             NCC_obtained_max = np.max(NCC_obtained)
             # Take highest NCC value of all templates
             if NCC_obtained_max > NCC_max:
@@ -87,11 +93,14 @@ if __name__== '__main__':
         h_fin, w_fin = candidate_pixel[0], candidate_pixel[1]
 
         # Update initial conditions
-        c_in = (c_in[0] - (window_next - 1)//2 + w_fin, c_in[1] - (window_next - 1)//2 + h_fin)
-        previous_imgs[i%num_imgs] = imgsimplify(img_next, c_in, window_template)
+        c_in = (c_in[0] - (window_next - 1) // 2 + w_fin,
+                c_in[1] - (window_next - 1) // 2 + h_fin)
+        previous_imgs[i % num_imgs] = imgsimplify(img_next, c_in,
+                                                  window_template)
 
         # Save new coordinates in the .txt file
-        output.write("{}\t\t{}\t\t{}\n".format(files_names[i], c_in[0], c_in[1]))
+        output.write("{}\t\t{}\t\t{}\n".format(files_names[i], c_in[0],
+                                               c_in[1]))
         output.flush()
 
         #Visual check of the obtained results
