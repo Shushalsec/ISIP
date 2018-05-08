eae50d36acc3bc6c6c80e1424a762c8a06c7805b
Madleina <madleina.caduff@gmx.ch>
2018-05-08 14:14:22 +0200

    made some manicure again :-)

--- ncc.py
+++ ncc.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 # INTRODUCTION TO SIGNAL AND IMAGE PROCESSING 2018 - GROUP PROJECT
+
 # TRACKING SURGICAL INSTRUMENTS USING NCC
-# Shushan Toneyan, Madleina Caduff, Judith Bergada Pijuan
+
+# Shushan Toneyan, Madleina Caduff, Judith Bergadà Pijuan
 
 from glob import glob
-
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.color import rgb2gray
@@ -14,19 +15,11 @@ from skimage.feature import match_template
 
 def imgsimplify(img, c, window):
     """
-    Returns a patch of the image
-    Parameters
-    ----------
-    img : array_like
-        Image that we want to cut or simplify
-    c : tuple
-        Center of the new image
-    window : integer
-        Size of the desired final image. It must be an odd number.
-    Returns
-    -------
-    simple_img : array_like
-        Image of size window x window that results from cutting the initial img.
+    Returns the part of the image
+    :param img: array_like; image that we want to cut or simplify
+    :param c: tuple; center of the new image
+    :param window: odd integer; size of the desired final image.
+    :return: array_like; image of size window x window that results from cutting the initial img.
     """
     c_x, c_y = c[0], c[1]
     window_half = (window - 1) // 2
@@ -36,17 +29,17 @@ def imgsimplify(img, c, window):
 
 
 if __name__ == '__main__':
-    #_______________________________________________________________
+    # _______________________________________________________________
     # Activate to work only with set A
-    files_names = glob("./project_data/a/*.png")
-    c_in = 348, 191
-    output = open('output_a.txt', 'w')
-    #_______________________________________________________________
+    # files_names = glob("./project_data/a/*.png")
+    # c_in = (348, 191)
+    # output = open('output_a.txt', 'w')
+    # _______________________________________________________________
     # Activate to work only with set B
-    # files_names = glob("./project_data/b/*.png")
-    # c_in = 439, 272
-    # output = open('output_b.txt', 'w')
-    #_______________________________________________________________
+    files_names = glob("./project_data/b/*.png")
+    c_in = (439, 272)
+    output = open('output_b.txt', 'w')
+    # _______________________________________________________________
 
     files_names.sort()
 
@@ -56,8 +49,7 @@ if __name__ == '__main__':
     window_template = window_next // 2
 
     # Initial conditions
-    img = rgb2gray(plt.imread(
-        files_names[0]))  # read first image with known starting positions
+    img = rgb2gray(plt.imread(files_names[0]))  # read first image with known starting positions
     previous_imgs = np.zeros((num_imgs, window_template, window_template))
     previous_imgs[0, :, :] = imgsimplify(img, c_in, window_template)
 
@@ -75,7 +67,7 @@ if __name__ == '__main__':
 
         # Compute NCC considering the previous frames as templates
         NCC_max = 0
-        for template in previous_imgs[0:min(i, len(previous_imgs))]:
+        for template in previous_imgs[0: min(i, len(previous_imgs))]:
             NCC_obtained = match_template(
                 simple_imgnext, template, pad_input=True, mode="edge")
             NCC_obtained_max = np.max(NCC_obtained)
@@ -92,15 +84,14 @@ if __name__ == '__main__':
         # Update initial conditions
         c_in = (c_in[0] - (window_next - 1) // 2 + w_fin,
                 c_in[1] - (window_next - 1) // 2 + h_fin)
-        previous_imgs[i % num_imgs] = imgsimplify(img_next, c_in,
-                                                  window_template)
+        previous_imgs[i % num_imgs] = imgsimplify(img_next, c_in, window_template)
 
         # Save new coordinates in the .txt file
         output.write("{}\t\t{}\t\t{}\n".format(files_names[i], c_in[0],
                                                c_in[1]))
         output.flush()
 
-        #Visual check of the obtained results
+        # Visual check of the obtained results
         plt.ion()
         plt.clf()
         plt.imshow(img_to_plot)
